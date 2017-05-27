9abc616f2981c00c87db8bf5354ea4c68d44a321
jcblw <jacoblowe2.0@gmail.com>
2017-05-27 16:41:28 -0700

    add full screen canvas stream

--- src/index.js
+++ src/index.js
@@ -4,44 +4,60 @@ const getusermedia = require('getusermedia')
 const findCorners = require('./find-corners')
 const centerOfTriangle = require('./center-of-triangle')
 
-const { div, input, canvas, h1, h3 } = domla
+const RESOLUTION = [640, 320]
+const [WIDTH, HEIGHT] = RESOLUTION
+// const RATIO = HEIGHT / WIDTH
+const POINT_AMOUNT = 1000
 
-const el = canvas()
+const { div, canvas, video } = domla
+
+const el = canvas({style: 'visibility:hidden;'})
 const context = el.getContext('2d')
-const fileInput = input({ type: 'file' })
+const largeEl = canvas({style: 'position:fixed;top:0;left:0;'})
+const largeContext = largeEl.getContext('2d')
 const content = div()
 const status = div()
+const videoEl = video({style: 'width:640px;height:360px;visibility:hidden;'})
 const layout = div({ className: 'textAlign-center container' },
-  h1({}, 'Delaunay Me'),
-  h3({}, 'Upload your own image to add a delaunay effect'),
-  fileInput,
-  status,
-  content
+  el,
+  largeEl,
+  videoEl
 )
 
 let mediaStream
+const mediaOptions = {
+  video: {
+    mandatory: {
+      maxWidth: WIDTH,
+      maxHeight: HEIGHT
+    }
+  }
+}
 
-getusermedia(function (err, stream) {
+getusermedia(mediaOptions, function (err, stream) {
   if (err) alert(err.message)
   mediaStream = stream
 })
-
-fileInput.addEventListener('change', function () {
-  const file = this.files[0]
-  loadImage(URL.createObjectURL(file), onImageLoad)
-}, false)
 
 function onImageLoad (err, img) {
   if (err) {
     status.innerText = err.message
     return
   }
-  status.innerText = 'appling effect'
-  el.width = img.width
-  el.height = img.height
-  context.drawImage(img, 0, 0)
-  const corners = findCorners(el, 1, 10000)
-  const coords = corners.map(({x, y}) => [x, y])
+  el.width = WIDTH / 2
+  el.height = HEIGHT / 2
+  largeEl.width = window.innerWidth
+  largeEl.height = window.innerHeight
+  const ratioY = largeEl.height / el.height
+  const ratioX = largeEl.width / el.width
+
+  largeContext.clearRect(0, 0, largeEl.width, largeEl.height)
+  context.clearRect(0, 0, el.width, el.height)
+  context.translate(el.width, 0)
+  context.scale(-1, 1)
+  context.drawImage(img, 0, 0, el.width, el.height)
+  const corners = findCorners(el, 1, POINT_AMOUNT)
+  const coords = corners.map(({x, y}) => [x * ratioX, y * ratioY])
   const triangles = new Delaunator(coords).triangles
   const build = []
   for (let i = 0; i < triangles.length; i += 3) {
@@ -52,22 +68,25 @@ function onImageLoad (err, img) {
     let x2 = coords[triangles[i + 2]][0]
     let y2 = coords[triangles[i + 2]][1]
     let center = centerOfTriangle(x0, y0, x1, y1, x2, y2)
-    let pixel = context.getImageData(center[0], center[1], 1, 1)
+    let pixel = context.getImageData(center[0] / ratioX, center[1] / ratioY, 1, 1)
     let data = pixel.data
     let rgb = `rgb(${data[0]}, ${data[1]}, ${data[2]})`
     build.push(function () {
-      context.beginPath()
-      context.moveTo(x0, y0)
-      context.lineTo(x1, y1)
-      context.lineTo(x2, y2)
-      context.fillStyle = rgb
-      context.fill()
+      largeContext.beginPath()
+      largeContext.moveTo(x0, y0)
+      largeContext.lineTo(x1, y1)
+      largeContext.lineTo(x2, y2)
+      largeContext.fillStyle = rgb
+      largeContext.fill()
     })
   }
   context.clearRect(0, 0, el.width, el.height)
   build.forEach(fn => fn())
-  const url = el.toDataURL('image/png')
-  const newImg = new Image()
+}
+
+function captureImage () {
+  const url = largeEl.toDataURL('image/png')
+  const newImg = new window.Image()
   newImg.src = url
   newImg.style = 'max-width:100%'
   status.innerText = ''
@@ -77,7 +96,7 @@ function onImageLoad (err, img) {
 
 function loadImage (src, callback) {
   status.innerText = 'loading image'
-  const img = new Image()
+  const img = new window.Image()
   img.src = src
   img.onload = callback.bind(null, null, img)
   img.onerror = callback
@@ -88,11 +107,15 @@ document.addEventListener('DOMContentLoaded', function () {
 })
 
 function draw () {
-  if (mediaStream) {
-
+  if (mediaStream && !videoEl.src) {
+    videoEl.src = window.URL.createObjectURL(mediaStream)
+    videoEl.play()
   }
-  setTimeout(
-    requestAnimationFrame(draw),
-    0
-  )
+
+  if (videoEl.src) {
+    onImageLoad(null, videoEl)
+  }
+  window.requestAnimationFrame(draw)
 }
+
+draw()
